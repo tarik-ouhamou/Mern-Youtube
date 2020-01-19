5271c7fe9710a752655a8dc8614e2f4e8598b181
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2020-01-19 03:59:42 +0100

    fix bug of protected routes

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,20 +5,30 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Redirect} from 'react-router-dom';
 
+
 class Home extends Component {
     constructor(props){
         super(props);
         this.state={
             url:'',
-            isAuthenticated:this.props.isAuthenticated
+            stay:0,
+            time:0
         }
     }
     componentDidUpdate(prevProps){
-        if(this.props.isAuthenticated!==prevProps.isAuthenticated){
-            this.setState({isAuthenticated:!this.state.isAuthenticated});
-
+        if(!this.props.isAuthenticated && this.props.finish){
+            this.setState({
+                stay:1
+            });
+        }
+        else if(this.props.isAuthenticated && this.props.finish && this.state.time!=1){
+            this.setState({
+                stay:2,
+                time:1
+            });
         }
     }
+
     onSubmitForm=(e)=>{
         e.preventDefault();
         const newUrl={
@@ -34,23 +44,35 @@ class Home extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <Container>
-                    <Form onSubmit={this.onSubmitForm}>
-                        <FormGroup>
-                            <h3 className="text-center" style={{marginBottom:'2rem'}}>Download One Video</h3>
-                            <Input type="text" name="url" id="url" placeholder="Enter youtube video url" onChange={this.onChangeUrl} />
-                            <Button color="dark" style={{marginTop:'2rem'}} block>Download</Button>
-                            {this.props.home.loading && <p className="text-center" style={{marginTop:"2rem"}}><i className="fa fa-refresh fa-spin" style={{fontSize:"80px",color:"#00bfff"}}></i><br /><span>Genrating Link</span></p>}
-                        </FormGroup>
-                    </Form>
-                    {this.props.home.linkMp3 && <div className="text-center"><a href={''+this.props.home.linkMp3+'.mp3'} download>Mp3 Link</a></div>}
-                    {this.props.home.linkMp4 && <div className="text-center"><a href={''+this.props.home.linkMp4+'.mp4'} download>Mp4 Link</a></div>}
-                    
-                </Container>
-            </div>
-        )
+        console.log(this.state.stay);
+        if(this.state.stay==2){
+            return (
+                <div>
+                    <Container>
+                        <Form onSubmit={this.onSubmitForm}>
+                            <FormGroup>
+                                <h3 className="text-center" style={{marginBottom:'2rem'}}>Download One Video</h3>
+                                <Input type="text" name="url" id="url" placeholder="Enter youtube video url" onChange={this.onChangeUrl} />
+                                <Button color="dark" style={{marginTop:'2rem'}} block>Download</Button>
+                                {this.props.home.loading && <p className="text-center" style={{marginTop:"2rem"}}><i className="fa fa-refresh fa-spin" style={{fontSize:"80px",color:"#00bfff"}}></i><br /><span>Genrating Link</span></p>}
+                            </FormGroup>
+                        </Form>
+                        {this.props.home.linkMp3 && <div className="text-center"><a href={''+this.props.home.linkMp3+'.mp3'} download>Mp3 Link</a></div>}
+                        {this.props.home.linkMp4 && <div className="text-center"><a href={''+this.props.home.linkMp4+'.mp4'} download>Mp4 Link</a></div>}       
+                    </Container>
+                </div>
+            )
+        }
+        else if(this.state.stay==1){
+            return(
+                <Redirect to="/" />
+            )
+        }
+        else{
+            return(
+                <div></div>
+            )
+        }
     }
 }
 
